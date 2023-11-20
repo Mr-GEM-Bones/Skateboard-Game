d7697d1216fcb63003d49c72d637859a23012412
TAbandija <guillermolopez08@gmail.com>
2023-11-20 11:45:26 -0400

    trashcan Obstacle
    -added trashcan obstacle.
    -refactored the speed out of obstacles
    -inheritance

--- scenes/Obstacles/obstacle.gd
+++ scenes/Obstacles/obstacle.gd
@@ -1,32 +1,23 @@
 extends RigidBody2D
 signal hit
 
-@export var move_speed = 120 # if the speed of the obstacles is different, this adds to it. 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	#print(position)
-	$AnimatedSprite2D.play("fly")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	var velocity = Vector2.LEFT * move_speed
-#	position += velocity*delta
-
-func _physics_process(delta):
-	#Set the velocity of the object. It should move in reference to main.speed with the relative motion.
-	var velocity = Vector2.LEFT * (get_parent().speed + move_speed)
+func move_obstacle_at_speed(offset_speed):
+	#Sets the velocity of the obstacle in relation to the main speed. Requieres obstacle to be child of Main.
+	var velocity = Vector2.LEFT * (get_parent().speed + offset_speed)
 	linear_velocity = velocity
 
 func _on_body_entered(body):
-	hit.emit()
-	queue_free()
+	#Adds funtionality to the obstacle hiting player.
+	if body.collision_layer == 1:
+		hit.emit()
+		queue_free()
 
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	#print("Bird Died")
+	# Obstacle deletes when it is of screen.
 	queue_free()
 
 
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var bird_obstacle_scene: PackedScene
+@export var ground_obstacle_scene: PackedScene
 
 var speed = 240 # the basic speed that everything moves at. Change this for everything to move.
 var spawnX = 782
@@ -22,6 +23,7 @@ func _on_bird_obstacle_hit():
 
 
 func _on_bird_timer_timeout():
+	#This timer handles all the obstacles on the sky
 	var bird = bird_obstacle_scene.instantiate()
 	var spawnY = 333
 	
@@ -29,3 +31,14 @@ func _on_bird_timer_timeout():
 	#print(bird)
 	
 	add_child(bird)
+
+
+func _on_ground_timer_timeout():
+	#This timer handles the obstacles on the ground.
+	var ground = ground_obstacle_scene.instantiate()
+	var spawnY = 415 #this is about as high, close to the floor.
+	
+	ground.position = Vector2(spawnX, spawnY)
+	
+	add_child(ground)
+	pass # Replace with function body.
