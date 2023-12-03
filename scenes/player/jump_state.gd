5f0f602fd04002741fc03253c8d651134a582882
TAbandija <guillermolopez08@gmail.com>
2023-12-03 18:46:18 -0400

    Skater can jump
    Made the skater jump
    jump animation is timed

--- scenes/player/jump_state.gd
+++ scenes/player/jump_state.gd
@@ -4,6 +4,4 @@ class_name JumpState
 
 func _ready():
 	animated_sprite.play("jump")
-	await get_tree().create_timer(1.0).timeout
-	#print("wait finished")
-	change_state.call_func("idle")
+
--- scenes/player/persistent_state.gd
+++ scenes/player/persistent_state.gd
@@ -8,6 +8,11 @@ var state_factory
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
+#Debuging variables
+var _timer = false
+var _current_timer = 0.0
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -28,10 +33,22 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	#DEBUG timer
+	if _timer:
+		_current_timer += delta
+		if is_on_floor():
+			_timer = false
+			print(_current_timer)
+			_current_timer = 0.0
+
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		_timer = true
 		velocity.y = JUMP_VELOCITY
-		change_state("hit")
+		change_state("jump")
+		await get_tree().create_timer(0.6).timeout
+		#print("wait finished")
+		change_state("idle")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
